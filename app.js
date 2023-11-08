62009036fc8c19bb95e19a99fdae4f7246c6fd73
Billkolos <billooko2@gmail.com>
2023-11-08 05:16:42 +0300

    The correct environment variable PORT is used to set the port on which the server listens, falling back to 3000 if the PORT environment variable is not set.
    The corsOptions variable is updated to use the PORT environment variable when defining the origin.
    
    The database initialization code is left intact as it was in your original code, but now it should use the environment variables correctly to connect to the database.

--- app.js
+++ app.js
@@ -1,44 +1,40 @@
 const express = require("express");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const app = express();
 
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
-app.set('views', __dirname+'/views')
+// Define the port
+const PORT = process.env.PORT || 3000;
 
-let corsOptions = {
-  origin: "http://localhost:${process.env.PORT}"
+// Import database models and initialize them
+const db = require("./models");
+db.syncDb(false).then(() => {
+  console.log('Database Synchronized!');
+});
+
+// Configure CORS options
+const corsOptions = {
+  origin: `http://localhost:${PORT}`
 };
 
-const db = require("./models");
-
-//SyncDb
-db.syncDb(false).then(() => {
-  // db.initiateRoles(db.Role)
-  console.log('Database Synchronized!')
-} )
-
+// Set up middleware
 app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
 app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Require and start all routes
-require('./routes')(app)
+require('./routes')(app);
 
-// set port, listen for requests
-const PORT = process.env.PORT || 3000;
-
-// app listen port
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}.`);
 });
+
